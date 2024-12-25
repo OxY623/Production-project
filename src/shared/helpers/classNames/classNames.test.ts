c1e95d069bec916b53e190d8b06f0b796f77540d
OxY623 <fuzzydoorproductions0@gmail.com>
2024-12-25 16:18:53 +0300

    fix: update tests classnames.test.ts

--- src/shared/helpers/classNames/classNames.test.ts
+++ src/shared/helpers/classNames/classNames.test.ts
@@ -1,17 +1,57 @@
 import {classNames} from "./classNames";
 
-describe("classnames", () => {
-  test("test with some class", () => {
+describe("Тестирование функции classNames", () => {
+  // Тест с одним классом
+  test("test с одним классом", () => {
     expect(classNames("someClass")).toBe("someClass");
   });
 
-  test("test with mods & additional", () => {
-    expect(classNames("someClass", {a: true, b: false, c: "true"}, ["extra"])).toBe(
-      "someClass extra a c",
+  // Тест с несколькими классами
+  test("test с несколькими классами", () => {
+    expect(classNames("someClass1 someClass2")).toBe("someClass1 someClass2");
+  });
+
+  // Тест с модификаторами (mods)
+  test("test с mods", () => {
+    expect(classNames("someClass", {a: true, b: false, c: "true"}, [])).toBe("someClass a c");
+  });
+
+  // Тест с дополнительными классами (additional)
+  test("test с дополнительными классами", () => {
+    expect(classNames("", {}, ["extra"])).toBe("extra");
+  });
+
+  // Тест с модификаторами, где некоторые значения false
+  test("test с модификаторами, где некоторые значения false", () => {
+    expect(classNames("baseClass", {active: false, disabled: true})).toBe("baseClass disabled");
+  });
+
+  // Тест с пустыми значениями (когда пустая строка, пустой объект и пустой массив)
+  test("test с пустыми значениями", () => {
+    expect(classNames("", {}, [])).toBe("");
+  });
+
+  // Тест с булевыми значениями в объекте
+  test("test с булевыми значениями", () => {
+    expect(classNames("baseClass", {active: true, disabled: false})).toBe("baseClass active");
+  });
+
+  // Тест с объектами с дополнительными классами
+  test("test с дополнительными классами и объектами", () => {
+    expect(classNames("baseClass", {active: true, disabled: true}, ["extraClass"])).toBe(
+      "baseClass extraClass active disabled",
     );
   });
 
-  test("test with additional", () => {
-    expect(classNames("", {}, ["extra"])).toBe("extra");
+  //   // Тест с null и undefined в классе
+  //   test("test с null и undefined", () => {
+  //     expect(classNames("baseClass", {active: true, disabled: null}, ["extraClass"])).toBe(
+  //       "baseClass active extraClass",
+  //     );
+  //   });
+
+  // Тест со строками в объекте
+  test("test с числовыми значениями", () => {
+    expect(classNames("baseClass", {active: "1", disabled: ""})).toBe("baseClass active");
   });
 });
