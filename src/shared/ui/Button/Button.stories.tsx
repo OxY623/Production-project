f0640aa228b318f4953d0f0e29f1cf9116ffa54d
OxY623 <fuzzydoorproductions0@gmail.com>
2025-01-11 21:53:20 +0300

    test: test CI

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -4,33 +4,20 @@ import {ThemeButton} from "./Button.types";
 import ThemeDecorator from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import {Theme} from "app/providers/ThemeProviders/ui/ThemeContext";
 
-// Метаданные Storybook для компонента Button
 const meta: Meta<typeof Button> = {
   title: "shared/Button",
   component: Button,
-  argTypes: {
-    backgroundColor: {control: "color"}, // Возможность менять цвет фона через Storybook
-    theme: {
-      control: "radio",
-      options: Object.values(ThemeButton), // Автоматически генерирует список тем из ThemeButton
-    },
-    onClick: {action: "clicked"}, // Добавляем действие для клика
-  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-// Основные истории
-
-// История с базовыми параметрами
 export const Primary: Story = {
   args: {
     children: "Primary Button",
   },
 };
 
-// История с темой "CLEAR"
 export const Clear: Story = {
   args: {
     children: "Clear Button",
@@ -38,33 +25,30 @@ export const Clear: Story = {
   },
 };
 
-// История с темой "OUTLINE"
-export const Outline: Story = {
-  args: {
-    children: "Outline Button",
-    theme: ThemeButton.OUTLINE,
-  },
-};
+// export const Outline: Story = {
+//   args: {
+//     children: "Outline Button",
+//     theme: ThemeButton.OUTLINE,
+//   },
+// };
 
-// История с темой Dark
-export const PrimaryDark: Story = {
-  parameters: {theme: Theme.DARK},
-  decorators: [ThemeDecorator],
-  args: {
-    children: "Primary Dark",
-  },
-};
+// export const PrimaryDark: Story = {
+//   parameters: {theme: Theme.DARK},
+//   decorators: [ThemeDecorator],
+//   args: {
+//     children: "Primary Dark",
+//   },
+// };
 
-export const ClearDark: Story = {
-  parameters: {theme: Theme.DARK},
-  decorators: [ThemeDecorator],
-  args: {
-    children: "Clear Dark",
-    theme: ThemeButton.CLEAR,
-  },
-};
+// export const ClearDark: Story = {
+//   parameters: {theme: Theme.DARK},
+//   decorators: [ThemeDecorator],
+//   args: {
+//     children: "Clear Dark",
+//     theme: ThemeButton.CLEAR,
+//   },
+// };
 
-// История с темой Dark
 export const OutlineDark: Story = {
   parameters: {theme: Theme.DARK},
   decorators: [ThemeDecorator],
